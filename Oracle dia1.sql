674b7db602d02f4378aac13ae34f05fa250e5031
Jorgecsl92 <jc.salinaslopez7@gmail.com>
2025-03-24 20:23:34 +0100

    Oracle semana 1

---
+++ Oracle dia1.sql
@@ -0,0 +1,9 @@
+--TAMAÑO DE LETRA
+--COMENTARIOS
+/*
+MAS DE UNA LINEA DE COMENTARIO
+*/
+--no diferencia mayusculas de minusculas en las palabras clave
+
+SELECT * from DEPT;
+SELECT DEPT_NO, DNOMBRE, LOC FROM DEPT;
